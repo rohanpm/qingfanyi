da0010235ddf1e5f0793ee43a05b129a167e031f
Rohan McGovern <rohan@mcgovern.id.au>
2016-03-15 21:15:26 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name='qingfanyi',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/rohanpm/qingfanyi',
+    license='GPL',
+    author='Rohan McGovern',
+    author_email='rohan@mcgovern.id.au',
+    description='Chinese to English dictionary lookup tool',
+    long_description=('Qingfanyi is a Chinese to English dictionary/translation tool. '
+                      'It uses the AT-SPI accessibility APIs on Linux desktop systems '
+                      'to capture Chinese text on the currently active window and '
+                      'provide an interactive lookup on demand.'),
+    install_requires=['ewmh==0.1.3',
+                      #'pyatspi',
+                      # 'gi',
+                      'marisa_trie==0.7.2'],
+    entry_points={
+        'console_scripts': [
+            'qfy=qingfanyi.process.main:run'
+        ]
+    }
+)
